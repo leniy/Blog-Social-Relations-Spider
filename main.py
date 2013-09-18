47da39bfd8241891ad37ed466283a5f0aabd191a
leniy <leniytsan@gmail.com>
2013-09-18 14:16:25 +0800

    update

--- main.py
+++ main.py
@@ -81,27 +81,27 @@ def main():
 #main()
 
 try:
-    conn=MySQLdb.connect(host='localhost',user='root',passwd='leniy.org',port=3306)
-    cur=conn.cursor()
+	conn=MySQLdb.connect(host='localhost',user='root',passwd='leniy.org',port=3306)
+	cur=conn.cursor()
 
-    cur.execute('create database if not exists python')
-    conn.select_db('python')
-    cur.execute('create table test(id int,info varchar(20))')
-     
-    value=[1,'hi rollen']
-    cur.execute('insert into test values(%s,%s)',value)
-     
-    values=[]
-    for i in range(20):
-        values.append((i,'hi rollen'+str(i)))
-         
-    cur.executemany('insert into test values(%s,%s)',values)
- 
-    cur.execute('update test set info="I am rollen" where id=3')
- 
-    conn.commit()
-    cur.close()
-    conn.close()
- 
+	cur.execute('create database if not exists python')
+	conn.select_db('python')
+	cur.execute('create table test(id int,info varchar(20))')
+
+	value=[1,'hi rollen']
+	cur.execute('insert into test values(%s,%s)',value)
+
+	values=[]
+	for i in range(20):
+		values.append((i,'hi rollen'+str(i)))
+
+	cur.executemany('insert into test values(%s,%s)',values)
+
+	cur.execute('update test set info="I am rollen" where id=3')
+
+	conn.commit()
+	cur.close()
+	conn.close()
+
 except MySQLdb.Error,e:
-     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+	print "Mysql Error %d: %s" % (e.args[0], e.args[1])
